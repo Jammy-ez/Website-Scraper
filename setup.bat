b2ed6bcadbf60514278caa67603ae0ac1a85d01f
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-01-14 16:31:03 +0000

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,10 @@
+pip install requests
+pip install bs4
+pip install pyfiglet
+pip install tqdm
+pip install colorama
+py -m pip install requests
+py -m pip install bs4
+py -m pip install pyfiglet
+py -m pip install tqdm
+py -m pip install colorama
