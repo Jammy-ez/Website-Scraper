1545d93df8b47c7404e10f4dc4a3f9af93df501e
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-01-20 19:32:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 from bs4 import BeautifulSoup
 from os import system, name
 from tqdm import tqdm
-from tqdm import tqdm
 from colorama import Fore, Back, Style
 def clear():
     _ = system('cls')
@@ -19,6 +18,7 @@ TEXT2 = pyfiglet.figlet_format("SCRAPING", font="rectangles")
 time.sleep(1)
 print(Fore.BLUE + TEXT1)
 time.sleep(1)
+print("Made by jam")
 URL = input("Enter page to scrape from: ")
 print("If this does not work (Likely) change the user agent to yours")
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.465'}
